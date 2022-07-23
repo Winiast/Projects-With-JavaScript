88bee61b0af6bf5bcf7d3731bb38304792f23d31
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-07-23 02:09:43 -0300

    Estilização feita e subtração concluída

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,6 +1,11 @@
 const valorDaTela = document.getElementById("valor");
 const botaoSoma = document.getElementById("soma");
+const botaoSubtrair = document.getElementById("menos");
+const botaoDividir = document.getElementById("dividir");
+const botaoMultiplicar = document.getElementById("multiplicar");
 const botaoIgual = document.getElementById("igual");
+
+// DOM dos numeros do 0 ao 9
 
 const valor1 = document.querySelector(".valor-1");
 const valor2 = document.querySelector(".valor-2");
@@ -13,10 +18,15 @@ const valor8 = document.querySelector(".valor-8");
 const valor9 = document.querySelector(".valor-9");
 const valor0 = document.querySelector(".valor-0");
 
+//  Declaração das Variaveis
+
 let valorMostrado = "";
 let numerosVisual = "";
 let valores = [];
 let local;
+let trocaEntreOperacoes;
+
+// Declaração dos Botoes do 0 ao 9
 
 valor1.addEventListener("click", () => {
   valorMostrado += valor1.textContent;
@@ -90,19 +100,77 @@ botaoSoma.addEventListener("click", () => {
   valorDaTela.innerText = soma();
 });
 
-botaoIgual.addEventListener("click", () => {});
+botaoSubtrair.addEventListener("click", () => {
+  valorDaTela.innerText = subtracao();
+});
 
-function soma() {
+// Botao Igual
+
+botaoIgual.addEventListener("click", () => {
+  switch (trocaEntreOperacoes) {
+    case 1:
+      valorDaTela.innerText = soma(1);
+      break;
+    case 2:
+      valorDaTela.innerText = subtracao(1);
+  }
+});
+
+// Botao Soma
+
+function soma(flag) {
+  trocaEntreOperacoes = 1;
+  transformarEmInteiro();
+  if (valores.length == 2) {
+    let somados = valores[0] + valores[1];
+    valores = [];
+    valores.push(somados);
+    if (flag == 1) {
+      numerosVisual = somados;
+    } else {
+      numerosVisual = somados + " + ";
+    }
+  } else {
+    if (flag == 1) {
+      numerosVisual = somados;
+    } else {
+      numerosVisual += " + ";
+    }
+  }
+  return numerosVisual;
+}
+
+// Botao Subtração
+
+function subtracao(flag) {
+  trocaEntreOperacoes = 2;
+
+  transformarEmInteiro();
+  if (valores.length == 2) {
+    let subtraidos = valores[0] - valores[1];
+    valores = [];
+    valores.push(subtraidos);
+    if (flag == 1) {
+      numerosVisual = subtraidos;
+    } else {
+      numerosVisual = subtraidos + " - ";
+    }
+  } else {
+    if (flag == 1) {
+      numerosVisual = subtraidos;
+    } else {
+      numerosVisual += " - ";
+    }
+  }
+  return numerosVisual;
+}
+
+function divisao(flag) {}
+
+function multiplicacao(flag) {}
+
+function transformarEmInteiro() {
   local = parseInt(valorMostrado);
   valores.push(local);
   valorMostrado = "";
-  if (valores.length == 2) {
-    let somados = valores[0] + valores[1];
-    numerosVisual = somados + " + ";
-    valores = [];
-    valores.push(somados);
-  } else {
-    numerosVisual += " + ";
-  }
-  return numerosVisual;
 }
