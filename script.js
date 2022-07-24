bfbc32652affdd857396ba838c7f896869100466
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-07-23 21:56:11 -0300

    Modificaçoes na calculadora

---
+++ script.js
@@ -0,0 +1,23 @@
+// class Carros {
+//   marca = this.marca;
+//   valor = this.valor;
+//   anoCarro = this.anoCarro;
+//   proprietario = this.proprietario;
+
+//   constructor(valor, proprietario) {
+//     this.valor = valor;
+//     this.proprietario = proprietario;
+//   }
+
+//   getMarca() {
+//     return this.marca;
+//   }
+// }
+
+// Veiculo = new Carros(2000, "Seu zé");
+
+// Veiculo.marca = "Citroen";
+
+// console.log(Veiculo.getMarca());
+
+// console.log(Veiculo.proprietario);
